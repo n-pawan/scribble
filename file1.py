2658a10fd3e505dc28d74f113b991d98c6f6431b
Pawan N <77048712+n-pawan@users.noreply.github.com>
2022-02-19 01:50:05 +0530

    issue description

--- file1.py
+++ file1.py
@@ -5,4 +5,6 @@
 # wth is happening!
 # edit from web
 # another edit
+# there is one case which is messing up: when I push from the local non-origin branch, the local origin branch is getting updated :/
+# instead it should update, only when i pull
 print('test')
