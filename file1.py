660a24e0c67f7a335731735d25d3c0b1ea3b1e4d
Pawan N <pavi1881@gmail.com>
2022-02-19 21:30:01 +0530

    added few git commands

--- file1.py
+++ file1.py
@@ -11,9 +11,23 @@ git branch <branch-name>
 git branch --delete <branch-name>
 git switch <branch-name>
 git switch -
+git reset --HARD origin/master
 git log
+git tag v1.0
+git tag -a v1.1 -m 'tag message, -a is verbose'
+git tag -a v1.2 15027957951b64cf874c3557a0f3547bd83b3ff6
 git reflog
+git checkout sha1 <detached head> ;# used to go back to a commit and experiment and commit code and a branch created out of it and then that can be merged
 git rebase -i HEAD~3 <pick sha1 squash sha1 squash sha1>
 git cat-file d3e92575b7dd42c143e7c56aa71f968e6efbf3e0 -t
 git cat-file d3e92575b7dd42c143e7c56aa71f968e6efbf3e0 -p
+
+git checkout <file> ; will discard all the changes in the working area
+git checkout . ; discards all the changes in all the files in working area
+git reset --soft HEAD~1/sha1 <uncommit and changes in staging area and working tree>
+git reset --mixed HEAD~1/sha1 <uncommit and changes only in working tree>
+git reset --hard HEAD~1/sha1 <uncommit and changes cleaned up>
+git revert HEAD~1/sha1 -m 'revert command' ; need to be followed by git push
+
+
 
